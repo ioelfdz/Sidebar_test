719192f9edb5830da7b970baacec6110dd4c25ff
ioelfdz <ioelfdz@yahoo.com>
2024-09-22 03:17:21 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,11 @@
-import streamlit as st
+
+
+    import streamlit as st
 from PIL import Image, ImageEnhance
 import time
 import json
 import requests
 import base64
-
 
 
 # Streamlit Page Configuration
@@ -17,26 +18,28 @@ st.set_page_config(
         "Get help": "https://github.com/AdieLaine/Streamly",
         "Report a bug": "https://github.com/AdieLaine/Streamly",
         "About": """
-            ## Streamly Streamlit Assistant
-            ### Powered using GPT-4o-mini
+                ## Streamly Streamlit Assistant
+                ### Powered using GPT-4o-mini
 
-            **GitHub**: https://github.com/AdieLaine/
+                **GitHub**: https://github.com/AdieLaine/
 
-            The AI Assistant named, Streamly, aims to provide the latest updates from Streamlit,
-            generate code snippets for Streamlit widgets,
-            and answer questions about Streamlit's latest features, issues, and more.
-            Streamly has been trained on the latest Streamlit updates and documentation.
-        """
+                The AI Assistant named, Streamly, aims to provide the latest updates from Streamlit,
+                generate code snippets for Streamlit widgets,
+                and answer questions about Streamlit's latest features, issues, and more.
+                Streamly has been trained on the latest Streamlit updates and documentation.
+            """
     }
 )
 
 # Streamlit Title
 st.title("Streamly Streamlit Assistant")
 
+
 def main():
     """
     Display Streamlit updates and handle the chat interface.
-    
+    """
+
     # Insert custom CSS for glowing effect
     st.markdown(
         """
